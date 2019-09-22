1d9cdbf4c84cd82497f4847728dc3750fddd551d
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-09-22 16:54:56 +0600

    a lot of changes, foget to commit

--- day6/crud/includes/add_data.php
+++ day6/crud/includes/add_data.php
@@ -1,3 +1,10 @@
 <?php
+$db = mysqli_connect('localhost', 'root', '', 'test', '3307');
+extract($_POST);
+if(isset($_POST['submit'])){
+    $q = "INSERT INTO newdata (`name`, `email`) Values ('$fname', '$email')" ;
+    mysqli_query($db, $q);
+    header("Location: ../index.php");
+}
 
 ?>
--- day6/crud/index.php
+++ day6/crud/index.php
@@ -2,51 +2,77 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 m-auto p-auto">
-            <div class="main-title">
-                <form action="includes/add_data.php" method="post" enctype="multipart/form-data" class="form-group">
+            <div class="main-title" id="main-content">
+                <form action="includes/add_data.php" method="post" enctype="multipart/form-data" class="form-group" id="form1">
                     <div class="form-row">
                         <div class="col">
                             <label for="fname">First Name</label>
-                            <input type="text" name="fname" id="fname" class="form-control">
+                            <input type="text" name="fname" id="fname" value="" class="form-control" onchange="nameCheck()">
                         </div>
                         <div class="col">
                             <label for="lname">Last Name</label>
-                            <input type="text" name="lname" id="lname" class="form-control">
+                            <input type="text" name="lname" id="lname" value="" class="form-control" onchange="nameCheck()">
                         </div>
                     </div>
+                    <span id="wfname"></span>
                     <div class="form-row">
                         <div class="col">
                             <label for="email">Email Address</label>
-                            <input type="email" name="email" id="email" class="form-control">
+                            <input type="email" name="email" id="email" class="form-control" onchange="validateemail()" required>
+                        </div>
+                    </div>
+                    <span id="wemail"></span>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="password1">Password</label>
+                            <input type="password" name="password1" id="password1" class="form-control" onchange="passVal()" autocomplete="true" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="col">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" id="password" class="form-control">
+                            <label for="password2">Password again</label>
+                            <input type="password" name="password2" id="password2" class="form-control" onchange="passVal()" autocomplete="true" required>
                         </div>
                     </div>
+                    <span id="wpass"></span>
                     <div class="row">
                         <label for="gender" class="col pt-3">Gender</label>
-                        <div class="col pt-3"><input type="radio" name="gender" id="gender" checked> Male</div>
-                        <div class="col pt-3"><input type="radio" name="gender" id="genderF"> Female</div>
-                        <div class="col pt-3"><input type="radio" name="gender" id="genderO"> Others</div>
+                        <div class="col pt-3"><input type="radio" name="gender" value="male" id="gender" checked> Male</div>
+                        <div class="col pt-3"><input type="radio" name="gender" value="female" id="genderF"> Female</div>
+                        <div class="col pt-3"><input type="radio" name="gender" value="gender" id="genderO"> Others</div>
                     </div>
                     <div class="row pt-2">
                         <div class="col pt-2">
                             <label for="dob">Date of Birth</label>
                         </div>
                         <div class="col pt-2">
-                            <input type="date" name="dob" id="dob" class="form-control">
+                            <input type="date" name="dob" id="dob" class="form-control" onchange="checkForm()">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="edu">Education</label>
+                            <input type="text" name="education" id="edu" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="address">Address</label>
+                            <input type="text" name="address" id="address" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="bio">Bio</label>
+                            <input type="text" name="bio" id="bio" class="form-control">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col pt-3"><input type="checkbox" name="agr" id="agr"> Do you agree?</div>
+                        <div class="col pt-3"><input type="checkbox" name="agr" id="agr" onchange="document.querySelector('#submit').disabled = !this.checked;"> Do you agree?</div>
                     </div>
                     <div class="row">
                         <div class="col pt-4">
-                            <input type="submit" value="submit" name="submit" id="submit"
-                                class="btn btn-success btn-lg btn-block">
+                            <input type="submit" value="submit" name="submit" id="submit" class="btn btn-success btn-lg btn-block" disabled>
                         </div>
                     </div>
                 </form>
@@ -54,4 +80,7 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+
+</script>
 <?php include "includes/footer.php" ?>
